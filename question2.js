1684edc590859425dece8ab6e42cd428b8cde273
Kaya Overholt <overholtkaya@gmail.com>
2020-09-17 02:42:26 -0700

    add javascript functionality

---
+++ question2.js
@@ -0,0 +1,9 @@
+var cuteCat= prompt('Do you think cats are cute?');
+
+if( cuteCat = "yes") {
+    document.write('Glad you are here!');
+} else if(cutCat = "no") {
+    document.write('You are a disgrace.');
+} else {
+    document.write('Hmm.......');
+}
---
+++ time.js
@@ -0,0 +1,13 @@
+var adopt = prompt("Are you looking to adopt today?");
+
+if(adopt = "yes"){
+    prompt("Thank you! Please see our contact info at the bottom of the page!");
+} else if(adopt = "Yes") {
+    prompt("Thank you! Please see our contact info at the bottom of the page!");
+} else if(adopt = "no") {
+    prompt("If you change your mind, feel free to come back!");
+} else if(adopt = "No") {
+    prompt("If you change your mind, feel free to come back!");
+} else {
+    prompt("Please try again.");
+}
