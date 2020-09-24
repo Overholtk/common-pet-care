5a1c0a8f6b2cf2ae6b792fc5c68cae8c77789cbd
Kaya Overholt <overholtkaya@gmail.com>
2020-09-23 17:20:54 -0700

    added linked pages for each animal

--- interactive.js
+++ interactive.js
@@ -1,17 +1,17 @@
 'use strict'
 
-function getUserName(){
-    var userName = prompt('Please enter your name:');
-    document.write("Welcome " + userName);
-}
+// function getUserName(){
+//     var userName = prompt('Please enter your name:');
+//     document.write("Welcome " + userName);
+// }
 
-function isCatCute(){
-    var cuteCat= prompt('Do you think cats are cute?');
-        cuteCat.toLowerCase;
-        while( cuteCat != "yes"){
-            cuteCat = prompt("You are a disgrace, try again.");
-        } 
-}
+// function isCatCute(){
+//     var cuteCat= prompt('Do you think cats are cute?');
+//         cuteCat.toLowerCase;
+//         while( cuteCat != "yes"){
+//             cuteCat = prompt("You are a disgrace, try again.");
+//         } 
+// }
 
 
 function displayTime(){
@@ -27,20 +27,20 @@ function displayTime(){
     }    
 }
 
-function guessGame(){
-    var treatGuess = prompt("Meow! I want a number of salmon treats between one and ten, how many will you give me?");
-    var treatNum = Math.floor(Math.random( )* 10) + 1;
-   for(var i = 0; i < 5; i++){
-    if (treatGuess != treatNum && i === 4){
-        alert("Sorry, times up! Now I knock your coffee off the table.")
-    } else if(treatGuess > treatNum){
-        var treatGuess= prompt("I can't eat that many! Try again.");
-    } else if( treatGuess < treatNum){
-        var treatGuess = prompt("Not enough! Try again.");
-    } else if(treatGuess == treatNum){
-        alert("Purrrfect. Now  I'm satisfied."); 
-        break; 
-    }
-   }
-}
+// function guessGame(){
+//     var treatGuess = prompt("Meow! I want a number of salmon treats between one and ten, how many will you give me?");
+//     var treatNum = Math.floor(Math.random( )* 10) + 1;
+//    for(var i = 0; i < 5; i++){
+//     if (treatGuess != treatNum && i === 4){
+//         alert("Sorry, times up! Now I knock your coffee off the table.")
+//     } else if(treatGuess > treatNum){
+//         var treatGuess= prompt("I can't eat that many! Try again.");
+//     } else if( treatGuess < treatNum){
+//         var treatGuess = prompt("Not enough! Try again.");
+//     } else if(treatGuess == treatNum){
+//         alert("Purrrfect. Now  I'm satisfied."); 
+//         break; 
+//     }
+//    }
+// }
 
